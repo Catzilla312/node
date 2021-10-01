c08206c29bb51d42ec67db8d5560d09d1d3bf52a
LDR <catzilla312@gmail.com>
2021-10-01 16:28:08 +0600

    static folder and prep for dep

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(formidableMiddleware())
 app.use(cors())
 const posts = require("./routes/api/posts")
+//handle production
+if(process.env.NODE_ENV==="production"){
+    //static folder
+    app.use(express.static(__dirname+'/public/'));
+    //handle spa
+    app.get(/.*/,(req,res)=>res.sendFile(__dirname+'/public/index.html'));
+}
 app.use("/api/posts",posts)
 const port = process.env.PORT || 5000
 app.listen(port,()=>{console.log(`Listening on port:${port}`)})
